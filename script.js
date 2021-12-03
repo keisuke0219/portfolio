a40ece7fd030f38b11bc37f124b6a61ea8c7f0fe
keisuke <nakou9139@icloud.com>
2021-12-03 18:07:55 +0900

    update

--- script.js
+++ script.js
@@ -13,10 +13,29 @@ $(window).on("scroll", function() {
     var elem_pos = $(this).offset().top;
     
     if (scroll_top >= elem_pos - window_h + 100) {
-      $(this).addClass("fadein");
+      $(this).addClass("fadeIn");
     } else {
-      $(this).removeClass("fadein");
+      $(this).removeClass("fadeIn");
       
     }
   });
 });
+
+$(function() {
+  var $nav   = $('#navArea');
+  var $btn   = $('.toggle_btn');
+  var $mask  = $('#mask');
+  var open   = 'open'; // class
+  // menu open close
+  $btn.on( 'click', function() {
+    if ( ! $nav.hasClass( open ) ) {
+      $nav.addClass( open );
+    } else {
+      $nav.removeClass( open );
+    }
+  });
+  // mask close
+  $mask.on('click', function() {
+    $nav.removeClass( open );
+  });
+} );
